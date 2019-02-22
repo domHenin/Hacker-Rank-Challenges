e71f67dd4cafb766974e61d9dbf29d7eaa18bb16
domHenin <domhenin.517@gmail.com>
2019-02-22 10:48:08 -0500

    initial setup

---
+++ src/com/hackerrank/challenges/daySix/DaySixMain.java
@@ -0,0 +1,43 @@
+package com.hackerrank.challenges.daySix;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class DaySixMain {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+    }
+}
+
+//TODO
+// >Objective:
+// Today we're expanding our knowledge of Strings and combining it with what
+// we've already learned about loops. Check out the Tutorial tab for learning
+// materials and an instructional video!
+// >Task:
+//  >>Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters
+//  as 2 space-separated strings on a single line (see the Sample below for more detail).
+// >Note: 0 is considered to be an even index.
+// >Input Format
+// >>The first line contains an integer, T (the number of test cases).
+// Each line i of the T subsequent lines contain a String, S.
+// Constraints:
+// 1<= T <= 10
+// 2<= length of S <= 10000
+// >Output Format
+// >> For each String S(j) (where 0 <= j <= T - 1), print S(j)'s even-indexed characters, followed by a space,
+// followed by S(j)'s odd-indexed characters.
+// Sample Input
+// 2
+// Hacker
+// Rank
+// Sample Output
+// Hce akr
+// Rn ak
+
+
