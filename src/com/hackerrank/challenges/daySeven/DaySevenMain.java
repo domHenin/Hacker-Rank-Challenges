dfc993ee2e13b4906b5804f601e2138ec2dcb496
domHenin <domhenin.517@gmail.com>
2019-09-10 15:36:13 -0400

    challenge complete

--- src/com/hackerrank/challenges/daySeven/DaySevenMain.java
+++ src/com/hackerrank/challenges/daySeven/DaySevenMain.java
@@ -14,8 +14,17 @@ public class DaySevenMain {
         int[] arr = new int[n];
 
 
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        scanner.close();    }
+        for (int i = n-1; i >= 0; i--) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+            System.out.print(arr[i] + " ");
+        }
+
+        scanner.close();
+    }
 }
 
 //TODO
